4361e724b3783190cd4716d525cf4f37e12d552e
hirapark8823 <90318741+hirapark8823@users.noreply.github.com>
2021-11-26 23:27:28 +0900

    prototype v2

---
+++ prototype_v1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prototype_v1
+//
+//  Created by 平本尚寛 on 2021/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
